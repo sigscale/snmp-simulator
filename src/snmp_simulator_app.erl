f1f878666158eb9397cb023d5567ff587aef4e2b
Vance Shipley <vances@sigscale.org>
2020-01-07 11:55:40 +0800

    add application behaviour callback module

---
+++ src/snmp_simulator_app.erl
@@ -0,0 +1,276 @@
+%%% snmp_simulator_app.erl
+%%% vim: ts=3
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @copyright 2016 - 2020 SigScale Global Inc.
+%%% @end
+%%% Licensed under the Apache License, Version 2.0 (the "License");
+%%% you may not use this file except in compliance with the License.
+%%% You may obtain a copy of the License at
+%%%
+%%%     http://www.apache.org/licenses/LICENSE-2.0
+%%%
+%%% Unless required by applicable law or agreed to in writing, software
+%%% distributed under the License is distributed on an "AS IS" BASIS,
+%%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%%% See the License for the specific language governing permissions and
+%%% limitations under the License.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%% @doc This {@link //stdlib/application. application} behaviour callback
+%%%   module starts and stops the
+%%%   {@link //snmp_simulator. snmp_simulator} application.
+%%%
+-module(snmp_simulator_app).
+-copyright('Copyright (c) 2016 - 2020 SigScale Global Inc.').
+
+-behaviour(application).
+
+%% callbacks needed for application behaviour
+-export([start/2, stop/1, config_change/3]).
+%% optional callbacks for application behaviour
+-export([prep_stop/1, start_phase/3]).
+%% export the snmp_simulator_app private API for installation
+-export([install/0, install/1]).
+
+-define(WAITFORSCHEMA, 9000).
+-define(WAITFORTABLES, 9000).
+
+-include("snmp_simulator.hrl").
+
+%%----------------------------------------------------------------------
+%%  The snmp_simulator_app aplication callbacks
+%%----------------------------------------------------------------------
+
+-type start_type() :: normal | {takeover, node()} | {failover, node()}.
+-spec start(StartType, StartArgs) -> Result
+	when
+		StartType :: start_type(),
+		StartArgs :: term(),
+		Result :: {ok, pid()} | {ok, pid(), State} | {error, Reason},
+		State :: #state{},
+		Reason :: term().
+%% @doc Starts the application processes.
+start(normal = _StartType, _Args) ->
+	{ok, MibDir} = application:get_env(mib_dir),
+	{ok, BinDir} = application:get_env(bin_dir),
+	case create_dirs(MibDir, BinDir) of
+		ok ->
+			case catch load_all_mibs(MibDir, BinDir) of
+				ok ->
+					start1(normal, []);
+				{error, Reason} ->
+					error_logger:error_report(["SNMP Agent application failed to start",
+							{reason, Reason}, {module, ?MODULE}]),
+					{error, Reason}
+			end;
+					{error, Reason}
+		{error, Reason} ->
+			{error, Reason}
+	end.
+%% @hidden
+start1(normal = _StartType, _Args) ->
+	Tables = [],
+	case mnesia:wait_for_tables(Tables, ?WAITFORTABLES) of
+		ok ->
+			start2();
+		{timeout, BadTabList} ->
+			case force(BadTabList) of
+				ok ->
+					start2();
+				{error, Reason} ->
+					error_logger:error_report(["SNMP Agent application failed to start",
+							{reason, Reason}, {module, ?MODULE}]),
+						{error, Reason}
+			end;
+		{error, Reason} ->
+			{error, Reason}
+	end.
+%% @hidden
+start2() ->
+	case supervisor:start_link(snmp_simulator_sup, []) of
+		{ok, TopSup} ->
+			{ok, TopSup};
+		{error, Reason} ->
+			{error, Reason}
+	end.
+
+-spec start_phase(Phase, StartType, PhaseArgs) -> Result
+	when
+		Phase :: atom(),
+		StartType :: start_type(),
+		PhaseArgs :: term(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Called for each start phase in the application and included
+%%   applications.
+%% @see //kernel/app
+%%
+start_phase(_Phase, _StartType, _PhaseArgs) ->
+	ok.
+
+-spec prep_stop(State) -> #state{}
+	when
+		State :: #state{}.
+%% @doc Called when the application is about to be shut down,
+%%   before any processes are terminated.
+%% @see //kernel/application:stop/1
+%%
+prep_stop(State) ->
+	State.
+
+-spec stop(State) -> any()
+	when
+		State :: #state{}.
+%% @doc Called after the application has stopped to clean up.
+%%
+stop(_State) ->
+	ok.
+
+-spec config_change(Changed, New, Removed) -> ok
+	when
+		Changed:: [{Par, Val}],
+		New :: [{Par, Val}],
+		Removed :: [Par],
+		Par :: atom(),
+		Val :: atom().
+%% @doc Called after a code  replacement, if there are any
+%%   changes to the configuration  parameters.
+%%
+config_change(_Changed, _New, _Removed) ->
+	ok.
+
+%%----------------------------------------------------------------------
+%%  Internal functions
+%%----------------------------------------------------------------------
+
+-spec install() -> Result
+	when
+		Result :: {ok, Tables},
+		Tables :: [atom()].
+%% @equiv install([node() | nodes()])
+install() ->
+	Nodes = [node() | nodes()],
+	install(Nodes).
+
+-spec install(Nodes) -> Result
+	when
+		Nodes :: [node()],
+		Result :: {ok, Tables},
+		Tables :: [atom()].
+%% @doc Initialize Snmp Collector Application tables.
+%% 	`Nodes' is a list of the nodes where
+%%
+%% 	If {@link //mnesia. mnesia} is not running an attempt
+%% 	will be made to create a schema on all available nodes.
+%% 	If a schema already exists on any node
+%% 	{@link //mnesia. mnesia} will be started on all nodes
+%% 	using the existing schema.
+%%
+%% @private
+%%
+install(Nodes) when is_list(Nodes) ->
+	case mnesia:system_info(is_running) of
+		no ->
+			case mnesia:create_schema(Nodes) of
+				ok ->
+					error_logger:info_report("Created mnesia schema",
+							[{nodes, Nodes}]),
+					install1(Nodes);
+				{error, Reason} ->
+					error_logger:error_report(["Failed to create schema",
+							mnesia:error_description(Reason),
+							{nodes, Nodes}, {error, Reason}]),
+					{error, Reason}
+			end;
+		_ ->
+			install2(Nodes)
+	end.
+%% @hidden
+install1([Node] = Nodes) when Node == node() ->
+	case mnesia:start() of
+		ok ->
+			error_logger:info_msg("Started mnesia~n"),
+			install2(Nodes);
+		{error, Reason} ->
+			error_logger:error_report([mnesia:error_description(Reason),
+					{error, Reason}]),
+			{error, Reason}
+	end;
+install1(Nodes) ->
+	case rpc:multicall(Nodes, mnesia, start, [], 50000) of
+		{Results, []} ->
+			F = fun(ok) ->
+						false;
+					(_) ->
+						true
+			end,
+			case lists:filter(F, Results) of
+				[] ->
+					error_logger:info_report(["Started mnesia on all nodes",
+							{nodes, Nodes}]),
+					install2(Nodes);
+				NotOKs ->
+					error_logger:error_report(["Failed to start mnesia"
+							" on all nodes", {nodes, Nodes}, {errors, NotOKs}]),
+					{error, NotOKs}
+			end;
+		{Results, BadNodes} ->
+			error_logger:error_report(["Failed to start mnesia"
+					" on all nodes", {nodes, Nodes}, {results, Results},
+					{badnodes, BadNodes}]),
+			{error, {Results, BadNodes}}
+	end.
+%% @hidden
+install2(_Nodes) ->
+	{ok, []}.
+
+-spec create_dirs(MibDir, BinDir) -> Result
+	when
+		MibDir :: string(),
+		BinDir :: string(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Create the MIB directory.
+create_dirs(MibDir, BinDir) ->
+	case file:make_dir(MibDir) of
+		ok ->
+			case file:make_dir(BinDir) of
+				ok ->
+					ok;
+				{error, eexist} ->
+					ok;
+				{error, Reason} ->
+					{error, Reason}
+			end;
+		{error, eexist} ->
+			ok;
+		{error, Reason} ->
+			{error, Reason}
+	end.
+
+-spec force(Tables) -> Result
+	when
+		Tables :: [TableName],
+		Result :: ok | {error, Reason},
+		TableName :: atom(),
+		Reason :: term().
+%% @doc Try to force load bad tables.
+force([H | T]) ->
+	case mnesia:force_load_table(H) of
+		yes ->
+			force(T);
+		ErrorDescription ->
+			{error, ErrorDescription}
+		end;
+force([]) ->
+	ok.
+
+-spec load_all_mibs(MibDir, BinDir) -> Result
+	when
+		MibDir :: string(),
+		BinDir :: string(),
+		Result :: ok | {error, Reason},
+		Reason :: term().
+%% @doc Load all required Mibs into the SNMP Agent.
+load_all_mibs(MibDir, BinDir) ->
+	ok.
+
