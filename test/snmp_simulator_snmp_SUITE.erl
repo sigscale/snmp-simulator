c42eeb1edff6c99f04582884c2e172e79b71f0eb
Vance Shipley <vances@sigscale.org>
2020-01-13 02:35:21 +0800

    test case for adding an alarm model

--- test/snmp_simulator_snmp_SUITE.erl
+++ test/snmp_simulator_snmp_SUITE.erl
@@ -130,14 +130,14 @@ sequences() ->
 %% Returns a list of all test cases in this test suite.
 %%
 all() ->
-	[get_model_last_changed].
+	[get_model_last_changed, add_model].
 
 %%---------------------------------------------------------------------
 %%  Test cases
 %%---------------------------------------------------------------------
 
 get_model_last_changed() ->
-	[{userdata, [{doc, "Test suite for SNMP agent in SigScale SNMP Simulator"}]}].
+	[{userdata, [{doc, "Get the time of the last model change."}]}].
 
 get_model_last_changed(_Config) ->
 	[User] = snmpm:which_users(),
@@ -147,6 +147,50 @@ get_model_last_changed(_Config) ->
 			value = Value}]}, _} = snmpm:sync_get(User, Agent,
 			[AlarmModelLastChanged ++ [0]]),
 	true = is_integer(Value).
+
+add_model() ->
+	[{userdata, [{doc, "Add an alarm model."}]}].
+
+add_model(_Config) ->
+	[User] = snmpm:which_users(),
+	[Agent] = snmpm:which_agents(),
+	{ok, [AlarmModelNotificationId]} = snmpm:name_to_oid(alarmModelNotificationId),
+	{ok, [AlarmModelVarbindIndex]} = snmpm:name_to_oid(alarmModelVarbindIndex),
+	{ok, [AlarmModelVarbindValue]} = snmpm:name_to_oid(alarmModelVarbindValue),
+	{ok, [AlarmModelDescription]} = snmpm:name_to_oid(alarmModelDescription),
+	{ok, [AlarmModelSpecificPointer]} = snmpm:name_to_oid(alarmModelSpecificPointer),
+	{ok, [AlarmModelVarbindSubtree]} = snmpm:name_to_oid(alarmModelVarbindSubtree),
+	{ok, [AlarmModelResourcePrefix]} = snmpm:name_to_oid(alarmModelResourcePrefix),
+	{ok, [AlarmModelRowStatus]} = snmpm:name_to_oid(alarmModelRowStatus),
+	{ok, [AlarmClearState]} = snmpm:name_to_oid(alarmClearState),
+	Index = [0, 1, 1],
+	OidVars = [{AlarmModelNotificationId ++ Index, AlarmClearState},
+			{AlarmModelVarbindIndex ++ Index, 0},
+			{AlarmModelVarbindValue ++ Index, 1},
+			{AlarmModelDescription ++ Index, "Clear Alarm"},
+			{AlarmModelSpecificPointer ++ Index, [0,0]},
+			{AlarmModelVarbindSubtree ++ Index, [0,0]},
+			{AlarmModelResourcePrefix ++ Index, [0,0]},
+			{AlarmModelRowStatus ++ Index, 4}],
+	{ok, {noError, _, VarBinds}, _} = snmpm:sync_set("simple_user",
+			"simulator", OidVars),
+	VarBinds = [#varbind{oid = AlarmModelNotificationId,
+					variabletype = 'OBJECT IDENTIFIER',
+					value = [1,3,6,1,2,1,118,0,3]},
+			#varbind{oid = AlarmModelVarbindIndex,
+					variabletype = 'Unsigned32', value = 0},
+			#varbind{oid = AlarmModelVarbindValue,
+					variabletype = 'INTEGER', value = 1},
+			#varbind{oid = AlarmModelDescription,
+					variabletype = 'OCTET STRING', value = "Clear Alarm"},
+			#varbind{oid = AlarmModelSpecificPointer,
+					variabletype = 'OBJECT IDENTIFIER', value = [0,0]},
+			#varbind{oid = AlarmModelVarbindSubtree,
+					variabletype = 'OBJECT IDENTIFIER', value = [0,0]},
+			#varbind{oid = AlarmModelResourcePrefix,
+					variabletype = 'OBJECT IDENTIFIER', value = [0,0]},
+			#varbind{oid = AlarmModelRowStatus,
+					variabletype = 'INTEGER', value = 4}].
 
 %%---------------------------------------------------------------------
 %%  Internal functions
