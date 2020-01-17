e011154c31e5d6312e911c713937ecfeeb3de0b7
Vance Shipley <vances@sigscale.org>
2020-01-17 14:08:00 +0800

    validate alarm models added through import

--- test/snmp_simulator_api_SUITE.erl
+++ test/snmp_simulator_api_SUITE.erl
@@ -28,6 +28,7 @@
 -compile(export_all).
 
 -include("snmp_simulator.hrl").
+-include_lib("snmp/include/snmp_types.hrl").
 -include_lib("common_test/include/ct.hrl").
 
 %%---------------------------------------------------------------------
@@ -128,10 +129,109 @@ import(Config) ->
 			"alarmActiveResourceId, undefined, critical, environmentalAlarm, "
 			"highTemperature, undefined}.", $\n],
 	ok = file:write_file(PrivDir ++ "/example-alarm-models", Chars),
-	ok = snmp_simulator_import:file(PrivDir ++ "/example-alarm-models").
-
+	ok = snmp_simulator_import:file(PrivDir ++ "/example-alarm-models"),
+	AlarmModelTable = {alarmModelTable, mnesia},
+	ItuAlarmTable = {ituAlarmTable, mnesia},
+	{value, AlarmClearState} = snmpa:name_to_oid(alarmClearState),
+	{value, AlarmActiveState} = snmpa:name_to_oid(alarmActiveState),
+	{value, ItuAlarmEventType} = snmpa:name_to_oid(ituAlarmEventType),
+	{value, AlarmActiveResourceId} = snmpa:name_to_oid(alarmActiveResourceId),
+	{value, AlarmModelNotificationId} = snmpa:name_to_oid(alarmModelNotificationId),
+	{value, AlarmModelRowStatus} = snmpa:name_to_oid(alarmModelRowStatus),
+	Index1 = snmp_generic:table_next(AlarmModelTable, []),
+	AlarmModelSpecificPointer1 = ItuAlarmEventType ++ itu_index(Index1),
+	[1, 1, AlarmClearState, 2, 1, "Temperature Normal",
+			AlarmModelSpecificPointer1, AlarmActiveResourceId, [0,0],
+			1] = snmp_generic:table_get_elements(AlarmModelTable,
+			Index1, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+	ItuAlarmGenericModel1 = AlarmModelNotificationId ++ Index1,
+	[1, 6, undefined, undefined,
+			ItuAlarmGenericModel1] = snmp_generic:table_get_elements(ItuAlarmTable,
+			itu_index(Index1), [1, 2, 3, 4, 5]),
+	Index2 = snmp_generic:table_next(AlarmModelTable, Index1),
+	AlarmModelSpecificPointer2 = ItuAlarmEventType ++ itu_index(Index2),
+	[2, 4, AlarmActiveState, 2, 4, "Temperature Low",
+			AlarmModelSpecificPointer2, AlarmActiveResourceId, [0,0],
+			1] = snmp_generic:table_get_elements(AlarmModelTable,
+			Index2, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+	ItuAlarmGenericModel2 = AlarmModelNotificationId ++ Index2,
+	[5, 6, 130, undefined,
+			ItuAlarmGenericModel2] = snmp_generic:table_get_elements(ItuAlarmTable,
+			itu_index(Index2), [1, 2, 3, 4, 5]),
+	Index3 = snmp_generic:table_next(AlarmModelTable, Index2),
+	AlarmModelSpecificPointer3 = ItuAlarmEventType ++ itu_index(Index3),
+	[3, 5, AlarmActiveState, 2, 5, "Temperature Low",
+			AlarmModelSpecificPointer3, AlarmActiveResourceId, [0,0],
+			1] = snmp_generic:table_get_elements(AlarmModelTable,
+			Index3, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+	ItuAlarmGenericModel3 = AlarmModelNotificationId ++ Index3,
+	[4, 6, 130, undefined,
+			ItuAlarmGenericModel3] = snmp_generic:table_get_elements(ItuAlarmTable,
+			itu_index(Index3), [1, 2, 3, 4, 5]),
+	Index4 = snmp_generic:table_next(AlarmModelTable, Index3),
+	AlarmModelSpecificPointer4 = ItuAlarmEventType ++ itu_index(Index4),
+	[4, 6, AlarmActiveState, 2, 6, "Temperature Low",
+			AlarmModelSpecificPointer4, AlarmActiveResourceId, [0,0],
+			1] = snmp_generic:table_get_elements(AlarmModelTable,
+			Index4, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+	ItuAlarmGenericModel4 = AlarmModelNotificationId ++ Index4,
+	[3, 6, 130, undefined,
+			ItuAlarmGenericModel4] = snmp_generic:table_get_elements(ItuAlarmTable,
+			itu_index(Index4), [1, 2, 3, 4, 5]),
+	Index5 = snmp_generic:table_next(AlarmModelTable, Index4),
+	AlarmModelSpecificPointer5 = ItuAlarmEventType ++ itu_index(Index5),
+	[5, 4, AlarmActiveState, 2, 4, "Temperature High",
+			AlarmModelSpecificPointer5, AlarmActiveResourceId, [0,0],
+			1] = snmp_generic:table_get_elements(AlarmModelTable,
+			Index5, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+	ItuAlarmGenericModel5 = AlarmModelNotificationId ++ Index5,
+	[5, 6, 123, undefined,
+			ItuAlarmGenericModel5] = snmp_generic:table_get_elements(ItuAlarmTable,
+			itu_index(Index5), [1, 2, 3, 4, 5]),
+	Index6 = snmp_generic:table_next(AlarmModelTable, Index5),
+	AlarmModelSpecificPointer6 = ItuAlarmEventType ++ itu_index(Index6),
+	[6, 5, AlarmActiveState, 2, 5, "Temperature High",
+			AlarmModelSpecificPointer6, AlarmActiveResourceId, [0,0],
+			1] = snmp_generic:table_get_elements(AlarmModelTable,
+			Index6, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+	ItuAlarmGenericModel6 = AlarmModelNotificationId ++ Index6,
+	[4, 6, 123, undefined,
+			ItuAlarmGenericModel6] = snmp_generic:table_get_elements(ItuAlarmTable,
+			itu_index(Index6), [1, 2, 3, 4, 5]),
+	Index7 = snmp_generic:table_next(AlarmModelTable, Index6),
+	AlarmModelSpecificPointer7 = ItuAlarmEventType ++ itu_index(Index7),
+	[7, 6, AlarmActiveState, 2, 6, "Temperature High",
+			AlarmModelSpecificPointer7, AlarmActiveResourceId, [0,0],
+			1] = snmp_generic:table_get_elements(AlarmModelTable,
+			Index7, [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]),
+	ItuAlarmGenericModel7 = AlarmModelNotificationId ++ Index7,
+	[3, 6, 123, undefined,
+			ItuAlarmGenericModel7] = snmp_generic:table_get_elements(ItuAlarmTable,
+			itu_index(Index7), [1, 2, 3, 4, 5]).
 
 %%---------------------------------------------------------------------
 %%  Internal functions
 %%---------------------------------------------------------------------
+
+-spec itu_index(AlarmModelIndex) -> ItuAlarmIndex
+	when
+		AlarmModelIndex :: [byte()],
+		ItuAlarmIndex :: [byte()].
+%% @doc Map AlarmModelTable RowIndex to ItuAlarmIndex RowIndex.
+%% @hidden
+itu_index(Index) ->
+	itu_index1(lists:split(length(Index) - 1, Index)).
+%% @hidden
+itu_index1({Prefix, [1]}) ->
+	Prefix ++ [1];
+itu_index1({Prefix, [2]}) ->
+	Prefix ++ [2];
+itu_index1({Prefix, [3]}) ->
+	Prefix ++ [6];
+itu_index1({Prefix, [4]}) ->
+	Prefix ++ [5];
+itu_index1({Prefix, [5]}) ->
+	Prefix ++ [4];
+itu_index1({Prefix, [6]}) ->
+	Prefix ++ [3].
 
